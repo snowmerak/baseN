a1c3e90062e8aa74aa7697165a1641475272d08a
snowmerak <snowmerak@outlook.com>
2024-12-13 02:07:55 +0900

    update example

--- main.go
+++ main.go
@@ -8,16 +8,28 @@ import (
 )
 
 func main() {
-	bs, err := base.New("?!")
+	bs, err := base.New("?!^#$")
 	if err != nil {
 		panic(err)
 	}
 
-	encoder := bs.NewEncoder(bytes.NewReader([]byte{0b10010010, 0b00100010, 0b10010011}))
+	origin := []byte{0b10010010, 0b00100010, 0b10010011}
+
+	encoder := bs.NewEncoder(bytes.NewReader(origin))
 	res, err := encoder.Encode()
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println(string(res))
+
+	buf := bytes.NewBuffer(nil)
+	decoder := bs.NewDecoder(buf)
+	decoded, err := decoder.Decode(res)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("origin: %08b\n", origin)
+	fmt.Printf("decoded: %08b\n", decoded)
 }
